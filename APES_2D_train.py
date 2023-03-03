d7533b2a9894cc6bbc49b825c902325a328ea4b6
wang <102914857+KGWANG2049@users.noreply.github.com>
2023-03-04 00:38:28 +0100

    Add files via upload

--- APES_2D_train.py
+++ APES_2D_train.py
@@ -1,45 +1,43 @@
 import os
-import time
 import random
+import time
 import numpy as np
+from path_gen import gmm_dist_generator
+import torch.distributions as dist
 import torch.nn.functional
 import torch.optim as optim
 from torch.optim import Adam
 from collections import deque
-import torch.distributions as dist
+from RRT_Connect_GMM import MyValidStateSampler
 from tensorboardX import SummaryWriter
 from torch.distributions import Dirichlet
 from mp2d.scripts.planning import Planning
-from apes.Dist_gen import gmm_dist_generator
 from mp2d.scripts.manipulator import manipulator
 from apes.network_2d import APESCriticNet, APESGeneratorNet
 from mp2d.scripts.utilities import load_planning_req_dataset
-from RRT_Connect_GMM import MyValidStateSampler
 
-BUFFER_MAX = 8
-REPLAY_SAMPLE_SIZE = 6
-EPOCH = 10
+BATCH_MAX = 64
+REPLAY_SAMPLE_SIZE = 32
+TRAIN_T = 100
 SAVE_INTERVAL = 50
 start = time.time()
 recent_steps = []
-TARGET_ENTROPY = [-20.0]
-LOG_ALPHA_INIT = [0.0]
-LR = 1e-3
-LOG_ALPHA_MIN = -10.
-LOG_ALPHA_MAX = 20.
+# TARGET_ENTROPY = [-150.0]
+# LOG_ALPHA_INIT = [-20.0]
+LR = 1e-4
+# LOG_ALPHA_MIN = -10.
+# LOG_ALPHA_MAX = 20.
 cwd = os.getcwd()
-easy_path = "/home/wang_ujsjo/Praktikum/apes/easy_pl_req_250_nodes.json"
+easy_path = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
 dataset = load_planning_req_dataset(easy_path)
 dof = 2
 links = [0.5, 0.5]
 ma = manipulator(dof, links)
 pl = Planning(ma)
-pl_req_file_name = "/home/wang_ujsjo/Praktikum/apes/easy_pl_req_250_nodes.json"
+pl_req_file_name = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
 planning_requests = load_planning_req_dataset(pl_req_file_name)
-replay_buffer = deque(maxlen=BUFFER_MAX)
+replay_buffer = deque(maxlen=BATCH_MAX)
 writer = SummaryWriter()
-
-
 if __name__ == '__main__':
 
     SV = np.array(2)
@@ -49,130 +47,138 @@ if __name__ == '__main__':
     pl_req = planning_requests[1]
     req = dataset[1]
     OC = np.array(np.shape(pl.get_occupancy_map(req)))
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     gen_model = APESGeneratorNet().double().to(device)
     gen_model.eval()
+    gen_model_optimizer = optim.Adam(gen_model.parameters(), lr=LR)
     critic_model = APESCriticNet().double().to(device)
     critic_model.eval()
-    gen_model_optimizer = optim.Adam(gen_model.parameters(), lr=LR)
-    critic_model_optimizer: Adam = optim.Adam(critic_model.parameters(), lr=LR)
-    log_alpha = torch.tensor(LOG_ALPHA_INIT, requires_grad=True, device=device)
-    alpha_optim = optim.Adam([log_alpha], lr=LR)
+    critic_model_optimizer = optim.Adam(critic_model.parameters(), lr=LR)
+    # log_alpha = torch.tensor(LOG_ALPHA_INIT, requires_grad=True, device=device)
+    # alpha_optim = optim.Adam([log_alpha], lr=LR)
     writer = SummaryWriter("LOSS_FUNCTION")
 
-    for e in range(EPOCH):
+    for e in range(60):
         replay_buffer.clear()
-
-        for i in range(0, BUFFER_MAX):
-            req = dataset[i]
-            OC = pl.get_occupancy_map(req)
-            OC = OC.reshape([1, OC.shape[0], -1])
+        for i in range(0, BATCH_MAX):
             pl_req = planning_requests[i]
             SV = pl_req.start
+            SV = torch.tensor(SV)
+            # print("start vektor:", SV)
+            # print("Original shape:", SV.shape)
             GV = pl_req.goal
+            GV = torch.tensor(GV)
+            # print("goal vektor:", GV)
+            # print("Original shape:", GV.shape)
+            req = dataset[i]
+            OC = pl.get_occupancy_map(req)
+            OC = torch.tensor(OC)
+            OC = OC.reshape([1, OC.shape[0], -1])
+            # print("occupancy map:", OC)
+            # print("Original shape:", OC.shape)
             pl.generate_graph_halton(150)
             pr = pl.search(req)
-            VALUE_ESTIMATE = pr.checked_counts
-            VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE)
-            # VALUE_ESTIMATE = plan().max_count
+            # VALUE_ESTIMATE = pr.checked_counts
             # VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE)
-            print("value_ESTIMATE", VALUE_ESTIMATE)
-            OC = torch.tensor(OC).to(device)
-            SV = torch.tensor(SV).to(device)
-            GV = torch.tensor(GV).to(device)
+            # print("Original shape:", VALUE_ESTIMATE.shape)
             W = gen_model(OC, SV, GV)
             gmm_dist = gmm_dist_generator(W)
-            W = torch.tensor(W).to(device)
-            MyValidStateSampler = MyValidStateSampler(si, gmm_dist)
-            VALUE_ESTIMATE = MyValidStateSampler.get_count_max()
+            W = torch.tensor(W)
+            # print("distribution weight:", W)
+            # print("Original shape:", W.shape)
+
+            MyValidStateSampler = MyValidStateSampler(gmm_dist, pl_req)
+            VALUE_ESTIMATE = MyValidStateSampler.plan()
+            print("VALUE_ESTIMATE", VALUE_ESTIMATE)
             experience = ([OC, SV, GV, W, VALUE_ESTIMATE])
+
             replay_buffer.append(experience)
-            print('Current Batch', e+1,  'Waiting for buffer size ... {}/{}'.format(len(replay_buffer), BUFFER_MAX))
+            print('Waiting for buffer size ... {}/{}'.format(len(replay_buffer), BATCH_MAX))
 
-        # labels = sampled_oc, sampled_start_v, sampled_goal_v, sampled_coefficients, sampled_values
-        sampled_evaluations = random.sample(replay_buffer, REPLAY_SAMPLE_SIZE)
-        # data_labels = {sampled_evaluations[i]: labels[i] for i in range(len(sampled_evaluations))}
-        sampled_oc = torch.stack([t[0] for t in sampled_evaluations])
-        # print("OC:", sampled_oc, sampled_oc.shape)
-        sampled_start_v = torch.stack([t[1] for t in sampled_evaluations])
-        # print("start_v:", sampled_start_v, sampled_start_v.shape)
-        sampled_goal_v = torch.stack([t[2] for t in sampled_evaluations])
-        # print("goal_v:", sampled_goal_v, sampled_goal_v.shape)
-        sampled_coefficients = torch.stack([t[3] for t in sampled_evaluations])
-        # print("sampled_coefficient:", sampled_coefficients, sampled_coefficients.shape)
-        sampled_values = torch.stack([t[4] for t in sampled_evaluations])
-        # print("value:", sampled_values, sampled_values.shape)
+        for i in range(TRAIN_T):
+            # labels = sampled_oc, sampled_start_v, sampled_goal_v, sampled_coefficients, sampled_values
+            sampled_evaluations = random.sample(replay_buffer, REPLAY_SAMPLE_SIZE)
+            # data_labels = {sampled_evaluations[i]: labels[i] for i in range(len(sampled_evaluations))}
+            sampled_oc = torch.stack([t[0] for t in sampled_evaluations])
+            # print("OC:", sampled_oc, sampled_oc.shape)
+            sampled_start_v = torch.stack([t[1] for t in sampled_evaluations])
+            # print("start_v:", sampled_start_v, sampled_start_v.shape)
+            sampled_goal_v = torch.stack([t[2] for t in sampled_evaluations])
+            # print("goal_v:", sampled_goal_v, sampled_goal_v.shape)
+            sampled_coefficients = torch.stack([t[3] for t in sampled_evaluations])
+            # print("sampled_coefficient:", sampled_coefficients, sampled_coefficients.shape)
+            sampled_values = torch.stack([t[4] for t in sampled_evaluations])
+            # print("value:", sampled_values, sampled_values.shape)
 
-        # update Cir
-        critic_loss = 0
+            # update Cir
+            critic_loss = 0
 
-        for j in range(REPLAY_SAMPLE_SIZE):
-            mean, std = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j], sampled_coefficients[j])
-            std = torch.exp(std)
-            # print("mean:", mean, "std:", std)
-            priori_pro = dist.Normal(mean, std)
-            # print("posterior:", priori_pro)
-            posterior_prob = priori_pro.log_prob(sampled_values[j])
-            # print("posterior_prob:", posterior_prob)
-            # print("test", priori_pro.log_prob(a).exp())
-            critic_loss = critic_loss + (-posterior_prob)
-        # print("critic_loss", critic_loss)
-        critic_model_optimizer.zero_grad()
-        gen_model_optimizer.zero_grad()
-        critic_loss.backward()
-        torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 1.0)
-        critic_model_optimizer.step()
+            for j in range(REPLAY_SAMPLE_SIZE):
+                (mean, std) = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j],
+                                           sampled_coefficients[j])
+                std = torch.exp(std)
+                # print("mean:", mean, "std:", std)
+                priori_pro = dist.Normal(mean, std)
+                # print("posterior:", priori_pro)
+                posterior_prob = priori_pro.log_prob(sampled_values[j])
+                # print("posterior_prob:", posterior_prob)
+                # print("test", priori_pro.log_prob(a).exp())
+                critic_loss = critic_loss + (-posterior_prob)
+            # print("critic_loss", critic_loss)
+            critic_model_optimizer.zero_grad()
+            gen_model_optimizer.zero_grad()
+            critic_loss.backward()
+            torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 1.0)
+            critic_model_optimizer.step()
 
-        # Update generator
-        gen_objective = 0
-        for j in range(REPLAY_SAMPLE_SIZE):
-            mean = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j], sampled_coefficients[j])[0]
-            # print("mean_gen", mean)
-            dir_dist = Dirichlet(sampled_coefficients[j])
-            entropy = dir_dist.entropy()
-            # print("entropy", entropy)
-            dual_terms = (log_alpha.exp().detach() * entropy)
-            # print("dual_term", dual_terms)
-            gen_objective = gen_objective + (mean - dual_terms)
-            # print("gen_objective", gen_objective)
-        # print("gen_objectivesum", gen_objective)
-        critic_model_optimizer.zero_grad()
-        gen_model_optimizer.zero_grad()
-        gen_objective.backward()
-        torch.nn.utils.clip_grad_norm_(gen_model.parameters(), 1.0)
-        gen_model_optimizer.step()
+            # Update generator
+            gen_objective = 0
+            for j in range(REPLAY_SAMPLE_SIZE):
+                mean = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j], sampled_coefficients[j])[0]
+                print("mean_gen", mean)
+                # dir_dist = Dirichlet(sampled_coefficients[j])
+                # entropy = dir_dist.entropy()
+                # print("entropy", entropy)
+                # dual_terms = (log_alpha.exp().detach() * entropy)
+                # print("dual_term", dual_terms)
+                gen_objective = gen_objective + mean
+                # print("gen_objective", gen_objective)
+            # print("gen_objectivesum", gen_objective)
+            critic_model_optimizer.zero_grad()
+            gen_model_optimizer.zero_grad()
+            gen_objective.backward()
+            torch.nn.utils.clip_grad_norm_(gen_model.parameters(), 1.0)
+            gen_model_optimizer.step()
 
-        # update alpha
-        alpha_loss = 0
-        for j in range(REPLAY_SAMPLE_SIZE):
-            dir_dist = Dirichlet(sampled_coefficients[j])
-            entropy = dir_dist.entropy()
+            # update alpha
+            """alpha_loss = 0
+            for j in range(REPLAY_SAMPLE_SIZE):
+                dir_dist = Dirichlet(sampled_coefficients[j])
+                entropy = dir_dist.entropy()
 
-            alpha_loss_single = log_alpha.exp() * ((entropy - torch.tensor(
-                TARGET_ENTROPY, device=device, dtype=torch.float32)).detach())
-            #  print("1", alpha_loss_single)
-            alpha_loss = alpha_loss + alpha_loss_single
-        # print("2", alpha_loss)
-        # print("3", alpha_loss)
-        alpha_optim.zero_grad()
-        alpha_loss.backward()
-        with torch.no_grad():
-            log_alpha.grad *= (((-log_alpha.grad >= 0) | (log_alpha >= LOG_ALPHA_MIN)) &
-                               ((-log_alpha.grad < 0) | (log_alpha <= LOG_ALPHA_MAX))).float()  # ppo
-        alpha_optim.step()
+                alpha_loss_single = log_alpha.exp() * ((entropy - torch.tensor(
+                    TARGET_ENTROPY, device=device, dtype=torch.float32)).detach())
+                #  print("1", alpha_loss_single)
+                alpha_loss = alpha_loss + alpha_loss_single
+            # print("2", alpha_loss)
+            # print("3", alpha_loss)
+            alpha_optim.zero_grad()
+            alpha_loss.backward()
+            with torch.no_grad():
+                log_alpha.grad *= (((-log_alpha.grad >= 0) | (log_alpha >= LOG_ALPHA_MIN)) &
+                                   ((-log_alpha.grad < 0) | (log_alpha <= LOG_ALPHA_MAX))).float()  # ppo
+            alpha_optim.step()"""
 
-        # critic_loss = critic_loss.detach().numpy()
-        critic_loss = critic_loss.item()
-        gen_objective = gen_objective.item()
-        alpha_loss = alpha_loss.item()
-        writer.add_scalar("CRITIC LOSS", critic_loss, i)
-        writer.add_scalar("GEN LOSS", gen_objective, i)
-        writer.add_scalar("ALPHA LOSS", alpha_loss, i)
+            # critic_loss = critic_loss.detach().numpy()
+            critic_loss = critic_loss.item()
+            gen_objective = gen_objective.item()
+            # alpha_loss = alpha_loss.item()
+            writer.add_scalar("CRITIC LOSS", critic_loss, i)
+            writer.add_scalar("GEN LOSS", gen_objective, i)
+            # writer.add_scalar("ALPHA LOSS", alpha_loss, i)
 
     torch.save(critic_model, 'net.critic')
     torch.save(gen_model, 'net.generator')
 
     writer.close()
     # tensorboard --logdir=/home/wangkaige/Project/apes/LOSS_FUNCTION
-
-
--- RRT_Connect_GMM.py
+++ RRT_Connect_GMM.py
@@ -1,6 +1,13 @@
-from mp2d.scripts.planning import *
+from mp2d.scripts.utilities import *
+from mp2d.scripts.rrt_connect import RRTConnect
 import matplotlib.pyplot as plt
 import numpy as np
+from time import sleep
+from math import fabs
+from path_gen import *
+import torch
+from mp2d.scripts.manipulator import *
+from mp2d.scripts.planning import *
 
 try:
     from ompl import util as ou
@@ -12,11 +19,11 @@ except ImportError:
     from os.path import abspath, dirname, join
     import sys
 
-    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), '/home/wang_ujsjo/Praktikum/mp2d/ompl-1.5.2/py'
-                                                                 '-bindings'))
+    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), '/home/wangkaige/Project/mp2d/ompl-1.5.2/py-bindings'))
     from ompl import util as ou
     from ompl import base as ob
     from ompl import geometric as og
+
 links = [0.5, 0.5]
 dof = 2
 ma = manipulator(dof, links)
@@ -28,20 +35,22 @@ pl_env = Planning(ma, planning_range_max, planning_range_min, resolution=0.05)
 
 class MyValidStateSampler(ob.ValidStateSampler):
 
-    def __init__(self, si, gmm_dist):
-        self.get_si()
-        super(MyValidStateSampler, self).__init__(si)
+    def __init__(self, gmm_dist, pl_req):
+        self.si = self.get_si()
+        super(MyValidStateSampler, self).__init__(self.si)
         self.name_ = "aaa"
-        self.gmm = gmm_dist
+        self.gmm_dist = gmm_dist
+        self.pl_req = pl_req
         self.count = 0
         self.max_count = 0
 
     def sample(self, state):
-        p = self.gmm.sample()
+        p = self.gmm_dist.sample()
         p = p.cpu().numpy()
         state[0] = p[0]
         state[1] = p[1]
         self.count += 1
+        print(self.count)
         if self.count > self.max_count:
             self.max_count = self.count
 
@@ -60,92 +69,94 @@ class MyValidStateSampler(ob.ValidStateSampler):
         self.si = ss.getSpaceInformation()
         return self.si
 
-    def __call__(self, _):
+    # def reset_counts(self):
+    # self.count = 0
+    def __call__(self, _, __):
         return self
 
+    def isStateValid(self, state):
+        obstacles = self.pl_req.obstacles
+        is_valid = pl_env.manipulator.check_validity(state, obstacles)
+        return is_valid
 
-def isStateValid(self, state):
-    obstacles = self.pl_req.obstacles
-    is_valid = pl_env.manipulator.check_validity(state, obstacles)
-    return is_valid
+    def plan(self):
+        # construct the state space we are planning in
+        space = ob.RealVectorStateSpace(2)
 
+        # set the bounds
+        bounds = ob.RealVectorBounds(2)
+        bounds.setLow(-4)
+        bounds.setHigh(4)
+        space.setBounds(bounds)
+        # define a simple setup class
+        ss = og.SimpleSetup(space)
+        # set state validity checking for this space
+        ss.setStateValidityChecker(ob.StateValidityCheckerFn(self.isStateValid))
 
-# This function is needed, even when we can write a sampler like the one
-# above, because we need to check path segments for validity
+        # create a start state
+        start = ob.State(space)
+        start[0] = self.pl_req.start[0]
+        start[1] = self.pl_req.start[1]
 
+        # create a goal state
+        goal = ob.State(space)
+        goal[0] = self.pl_req.goal[0]
+        goal[1] = self.pl_req.goal[1]
 
-def plan(state, pl_req):
-    # construct the state space we are planning in
-    space = ob.RealVectorStateSpace(2)
+        # set the start and goal states;
+        ss.setStartAndGoalStates(start, goal)
 
-    # set the bounds
-    bounds = ob.RealVectorBounds(2)
-    bounds.setLow(-4)
-    bounds.setHigh(4)
-    space.setBounds(bounds)
-    # define a simple setup class
-    ss = og.SimpleSetup(space)
-    # check path segments for validity
-    obstacles = pl_req.obstacles
-    isStateValid = pl_env.manipulator.check_validity(state, obstacles)
-    # set state validity checking for this space
-    ss.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
-    # create a start state
-    start = ob.State(space)
-    start[0] = pl_req.start[0]
-    start[1] = pl_req.start[1]
-    # create a goal state
-    goal = ob.State(space)
-    goal[0] = pl_req.goal[0]
-    goal[1] = pl_req.goal[1]
+        # set sampler (optional; the default is uniform sampling)
+        si = ss.getSpaceInformation()
+        sampler = MyValidStateSampler(self.gmm_dist, self.pl_req)
 
-    # set the start and goal states;
-    ss.setStartAndGoalStates(start, goal)
-    # set sampler (optional; the default is uniform sampling)
-    si = ss.getSpaceInformation()
-    sampler = MyValidStateSampler(si)
-    # use my sampler
-    si.setValidStateSamplerAllocator(ob.ValidStateSamplerAllocator(sampler))
-    # create a planner for the defined space
-    planner = og.RRTConnect(si)
-    ss.setPlanner(planner)
-    # attempt to solve the problem within 20 seconds of planning time
-    solved = ss.solve(20.0)
-    solution_path = ss.getSolutionPath()
-    ompl_solution = list(solution_path.getStates())
-    solution = []
-    max_count = sampler.get_count_max()
-    print("maxcount:", max_count)
-    for state in ompl_solution:
-        np_state = np.zeros(2)
-        np_state[0] = state[0]
-        np_state[1] = state[1]
-        solution.append(np_state)
+        # use my sampler
+        si.setValidStateSamplerAllocator(ob.ValidStateSamplerAllocator(sampler))
 
-    if solved:
-        print("Found solution:")
-        """ 
-        # print the path to screen
-        print(ss.getSolutionPath())
-        print(solution)
-        _, ax = plt.subplots(1, 1)
-        ax.scatter(samples[:, 0], samples[:, 1], s=1)
-        px = [node[0] for node in mean]
-        py = [node[1] for node in mean]
-        ax.scatter(px, py, color="red", s=10)
-        ax.scatter(req.start[0], req.start[1], color="green", s=60)
-        ax.scatter(req.goal[0], req.goal[1], color="blue", s=60)
-        obstacles_space = pl.get_obstacle_space(req)
-        obst_space_x = [ns[0] for ns in obstacles_space]
-        obst_space_y = [ns[1] for ns in obstacles_space]
-        ax.scatter(obst_space_x, obst_space_y, c="r", s=1)
-        x = [ns[0] for ns in solution]
-        y = [ns[1] for ns in solution]
-        plt.plot(x, y)
-        plt.show()
-        """
-    else:
-        print("No solution found")
+        # create a planner for the defined space
+        planner = og.RRTConnect(si)
+        ss.setPlanner(planner)
+
+        # attempt to solve the problem within 20 seconds of planning time
+        solved = ss.solve(20.0)
+        solution_path = ss.getSolutionPath()
+
+        ompl_solution = list(solution_path.getStates())
+        solution = []
+
+        for state in ompl_solution:
+            np_state = np.zeros(2)
+            np_state[0] = state[0]
+            np_state[1] = state[1]
+            solution.append(np_state)
+
+        if solved:
+            print("Found solution:")
+            # print the path to screen
+            print(ss.getSolutionPath())
+            print(solution)
+            """_, ax = plt.subplots(1, 1)
+            # ax.scatter(samples[:, 0], samples[:, 1], s=1)
+            # px = [node[0] for node in mean]
+            # py = [node[1] for node in mean]
+            # ax.scatter(px, py, color="red", s=10)
+            # ax.scatter(req.start[0], req.start[1], color="green", s=60)
+            # ax.scatter(req.goal[0], req.goal[1], color="blue", s=60)
+            obstacles_space = pl.get_obstacle_space(req)
+            obst_space_x = [ns[0] for ns in obstacles_space]
+            obst_space_y = [ns[1] for ns in obstacles_space]
+            ax.scatter(obst_space_x, obst_space_y, c="r", s=1)
+            x = [ns[0] for ns in solution]
+            y = [ns[1] for ns in solution]
+            plt.plot(x, y)
+            plt.show()"""
+
+        else:
+            print("No solution found")
+        return self.get_count_max()
+
+# max_count = MyValidStateSampler.get_count_max()
+# print("maxcount:", max_count)
 
 # if __name__ == '__main__':
 # plan()
