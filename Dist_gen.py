aeeffa4a76fb07cb6bf29e87708ecc8252d8d8d6
wang <102914857+KGWANG2049@users.noreply.github.com>
2023-03-03 17:00:04 +0100

    Add files via upload

---
+++ Dist_gen.py
@@ -0,0 +1,76 @@
+import torch
+import torch.distributions as td
+from mp2d.scripts.utilities import *
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+"""
+pl_req_file_name = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
+planning_requests = load_planning_req_dataset(pl_req_file_name)
+SV = np.array(2)
+GV = np.array(2)
+W = np.array(50)
+links = [0.5, 0.5]
+dof = 2
+ma = manipulator(dof, links)
+pl = Planning(ma)
+planning_range_max = np.array([np.pi, np.pi])
+planning_range_min = np.array([-np.pi, -np.pi])
+pl_env = Planning(ma, planning_range_max, planning_range_min, resolution=0.05)
+nodes_recording = {}
+planning_requests_wo_obstacles = deepcopy(planning_requests)
+# Find solutions in parallel using RRT
+easy_path = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
+dataset = load_planning_req_dataset(easy_path)
+# Define the weights for each multivariate Gaussian
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print("...")
+pl_req = planning_requests[813]
+SV = pl_req.start
+SV = torch.tensor(SV)
+GV = pl_req.goal
+GV = torch.tensor(GV)
+req = pl_req
+OC = pl.get_occupancy_map(req)
+OC = torch.tensor(OC)
+OC = OC.reshape([1, OC.shape[0], -1])
+# Gen_net = torch.load("/home/wangkaige/Project/apes/net.generator")
+Gen_net = gen_model
+W = Gen_net(OC, SV, GV)
+"""
+
+
+def gmm_dist_generator(W):
+    # print("WEIGHT", W)
+    length_W = W.shape[1]
+    W = torch.tensor(W)
+    W = W.squeeze()
+    # print(W)
+    # W = td.Categorical(torch.log(W).squeeze().exp())
+    # w = td.Categorical(log.w.squeeze().exp())
+    # print("cat", cat)
+    solution_list = []
+    path_w_idx = torch.load('/home/wang_ujsjo/Praktikum/apes/fixed_path_new')
+    # print(path_w_idx)
+    mean = torch.load('/home/wang_ujsjo/Praktikum/apes/mean_new')
+    # print(mean)
+    W_all = torch.zeros(path_w_idx[-1])
+    # print(W_all)
+    for i in range(length_W):
+        # pl_env.visualize_path(req, path.solution_path)
+        point_num = path_w_idx[i + 1] - path_w_idx[i]
+        W_line = torch.ones(point_num) * W[i] / point_num
+        W_all[path_w_idx[i]: path_w_idx[i + 1]] = W_line
+
+    W_sum = td.Categorical(torch.log(W_all).squeeze().exp())
+    # print("mean_sum:", mean)
+    # mean = torch.stack(mean).to(device)
+    # print(mean.shape)
+    cov = np.eye(2) * 0.1
+    cov = torch.tensor(cov)
+    # normal_dis = MultivariateNormal(mean, cov)  # Not sure about
+    dist = td.Independent(MultivariateNormal(mean, cov), 0)  # Not sure about 1
+    # print(dist)
+    gmm_dist = torch.distributions.MixtureSameFamily(W_sum, dist)
+    # samples = gmm_dist.sample([2000])
+    return gmm_dist
--- RRT_Connect_GMM.py
+++ RRT_Connect_GMM.py
@@ -1,13 +1,6 @@
-from mp2d.scripts.utilities import *
-from mp2d.scripts.rrt_connect import RRTConnect
+from mp2d.scripts.planning import *
 import matplotlib.pyplot as plt
 import numpy as np
-from time import sleep
-from math import fabs
-from path_gen import *
-import torch
-from mp2d.scripts.manipulator import *
-from mp2d.scripts.planning import *
 
 try:
     from ompl import util as ou
@@ -19,11 +12,11 @@ except ImportError:
     from os.path import abspath, dirname, join
     import sys
 
-    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), '/home/wangkaige/Project/mp2d/ompl-1.5.2/py-bindings'))
+    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), '/home/wang_ujsjo/Praktikum/mp2d/ompl-1.5.2/py'
+                                                                 '-bindings'))
     from ompl import util as ou
     from ompl import base as ob
     from ompl import geometric as og
-
 links = [0.5, 0.5]
 dof = 2
 ma = manipulator(dof, links)
@@ -36,6 +29,7 @@ pl_env = Planning(ma, planning_range_max, planning_range_min, resolution=0.05)
 class MyValidStateSampler(ob.ValidStateSampler):
 
     def __init__(self, si, gmm_dist):
+        self.get_si()
         super(MyValidStateSampler, self).__init__(si)
         self.name_ = "aaa"
         self.gmm = gmm_dist
@@ -56,39 +50,50 @@ class MyValidStateSampler(ob.ValidStateSampler):
     def get_count_max(self):
         return self.max_count
 
-    # def reset_counts(self):
-    # self.count = 0
+    def get_si(self):
+        space = ob.RealVectorStateSpace(2)
+        bounds = ob.RealVectorBounds(2)
+        bounds.setLow(-4)
+        bounds.setHigh(4)
+        space.setBounds(bounds)
+        ss = og.SimpleSetup(space)
+        self.si = ss.getSpaceInformation()
+        return self.si
+
     def __call__(self, _):
         return self
 
 
-def isStateValid(state):
-    obstacles = pl_req.obstacles
+def isStateValid(self, state):
+    obstacles = self.pl_req.obstacles
     is_valid = pl_env.manipulator.check_validity(state, obstacles)
     return is_valid
+
+
+# This function is needed, even when we can write a sampler like the one
+# above, because we need to check path segments for validity
 
 
 def plan(state, pl_req):
     # construct the state space we are planning in
     space = ob.RealVectorStateSpace(2)
+
     # set the bounds
     bounds = ob.RealVectorBounds(2)
     bounds.setLow(-4)
     bounds.setHigh(4)
     space.setBounds(bounds)
-    obstacles = pl_req.obstacles
-    is_valid = pl_env.manipulator.check_validity(state, obstacles)
-    isStateValid = is_valid
     # define a simple setup class
     ss = og.SimpleSetup(space)
+    # check path segments for validity
+    obstacles = pl_req.obstacles
+    isStateValid = pl_env.manipulator.check_validity(state, obstacles)
     # set state validity checking for this space
     ss.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
-
     # create a start state
     start = ob.State(space)
     start[0] = pl_req.start[0]
     start[1] = pl_req.start[1]
-
     # create a goal state
     goal = ob.State(space)
     goal[0] = pl_req.goal[0]
@@ -96,25 +101,21 @@ def plan(state, pl_req):
 
     # set the start and goal states;
     ss.setStartAndGoalStates(start, goal)
-
     # set sampler (optional; the default is uniform sampling)
     si = ss.getSpaceInformation()
     sampler = MyValidStateSampler(si)
-
     # use my sampler
     si.setValidStateSamplerAllocator(ob.ValidStateSamplerAllocator(sampler))
-
     # create a planner for the defined space
     planner = og.RRTConnect(si)
     ss.setPlanner(planner)
-
     # attempt to solve the problem within 20 seconds of planning time
     solved = ss.solve(20.0)
     solution_path = ss.getSolutionPath()
-
     ompl_solution = list(solution_path.getStates())
     solution = []
-
+    max_count = sampler.get_count_max()
+    print("maxcount:", max_count)
     for state in ompl_solution:
         np_state = np.zeros(2)
         np_state[0] = state[0]
@@ -123,16 +124,17 @@ def plan(state, pl_req):
 
     if solved:
         print("Found solution:")
+        """ 
         # print the path to screen
         print(ss.getSolutionPath())
         print(solution)
         _, ax = plt.subplots(1, 1)
-        # ax.scatter(samples[:, 0], samples[:, 1], s=1)
-        # px = [node[0] for node in mean]
-        # py = [node[1] for node in mean]
-        # ax.scatter(px, py, color="red", s=10)
-        # ax.scatter(req.start[0], req.start[1], color="green", s=60)
-        # ax.scatter(req.goal[0], req.goal[1], color="blue", s=60)
+        ax.scatter(samples[:, 0], samples[:, 1], s=1)
+        px = [node[0] for node in mean]
+        py = [node[1] for node in mean]
+        ax.scatter(px, py, color="red", s=10)
+        ax.scatter(req.start[0], req.start[1], color="green", s=60)
+        ax.scatter(req.goal[0], req.goal[1], color="blue", s=60)
         obstacles_space = pl.get_obstacle_space(req)
         obst_space_x = [ns[0] for ns in obstacles_space]
         obst_space_y = [ns[1] for ns in obstacles_space]
@@ -141,12 +143,9 @@ def plan(state, pl_req):
         y = [ns[1] for ns in solution]
         plt.plot(x, y)
         plt.show()
+        """
     else:
         print("No solution found")
 
-
-max_count = MyValidStateSampler.get_count_max()
-print("maxcount:", max_count)
-
-if __name__ == '__main__':
-    plan()
+# if __name__ == '__main__':
+# plan()
