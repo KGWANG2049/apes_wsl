2b8a7f0781a0fa97e75a586498ed210b4c8d7a0b
wang <102914857+KGWANG2049@users.noreply.github.com>
2023-03-12 23:01:06 +0100

    Add files via upload

--- APES_2D_train.py
+++ APES_2D_train.py
@@ -10,23 +10,25 @@ from torch.optim import Adam
 from collections import deque
 from RRT_Connect_GMM import MyValidStateSampler
 from tensorboardX import SummaryWriter
-from torch.distributions import Dirichlet
+from experience import plan
+from ran_rrtconnect import random_plan
 from mp2d.scripts.planning import Planning
 from mp2d.scripts.manipulator import manipulator
 from apes.network_2d import APESCriticNet, APESGeneratorNet
 from mp2d.scripts.utilities import load_planning_req_dataset
+from torch.distributions.dirichlet import Dirichlet
 
-BATCH_MAX = 64
-REPLAY_SAMPLE_SIZE = 32
+BUFFER_MAX = 10
+REPLAY_SAMPLE_SIZE = 2
 TRAIN_T = 100
 SAVE_INTERVAL = 50
 start = time.time()
 recent_steps = []
-# TARGET_ENTROPY = [-150.0]
-# LOG_ALPHA_INIT = [-20.0]
+TARGET_ENTROPY = [-150.0]
+LOG_ALPHA_INIT = [-20.0]
 LR = 1e-4
-# LOG_ALPHA_MIN = -10.
-# LOG_ALPHA_MAX = 20.
+LOG_ALPHA_MIN = -10.
+LOG_ALPHA_MAX = 20.
 cwd = os.getcwd()
 easy_path = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
 dataset = load_planning_req_dataset(easy_path)
@@ -36,7 +38,7 @@ ma = manipulator(dof, links)
 pl = Planning(ma)
 pl_req_file_name = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
 planning_requests = load_planning_req_dataset(pl_req_file_name)
-replay_buffer = deque(maxlen=BATCH_MAX)
+replay_buffer = deque(maxlen=BUFFER_MAX)
 writer = SummaryWriter()
 if __name__ == '__main__':
 
@@ -44,60 +46,54 @@ if __name__ == '__main__':
     GV = np.array(2)
     W = np.array(50)
     data = np.array([])
-    pl_req = planning_requests[1]
-    req = dataset[1]
-    OC = np.array(np.shape(pl.get_occupancy_map(req)))
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    OC = np.array(np.shape(pl.get_occupancy_map(planning_requests[1])))
+    device = torch.device("cpu")
     gen_model = APESGeneratorNet().double().to(device)
     gen_model.eval()
-    gen_model_optimizer = optim.Adam(gen_model.parameters(), lr=LR)
     critic_model = APESCriticNet().double().to(device)
     critic_model.eval()
-    critic_model_optimizer = optim.Adam(critic_model.parameters(), lr=LR)
-    # log_alpha = torch.tensor(LOG_ALPHA_INIT, requires_grad=True, device=device)
-    # alpha_optim = optim.Adam([log_alpha], lr=LR)
-    writer = SummaryWriter("LOSS_FUNCTION")
-
-    for e in range(60):
-        replay_buffer.clear()
-        for i in range(0, BATCH_MAX):
+    gen_model_optimizer: Adam = optim.Adam(gen_model.parameters(), lr=LR)
+    critic_model_optimizer: Adam = optim.Adam(critic_model.parameters(), lr=LR)
+    log_alpha = torch.tensor(LOG_ALPHA_INIT, requires_grad=True, device=device)
+    alpha_optim = optim.Adam([log_alpha], lr=LR)
+    torch.autograd.set_detect_anomaly(True)
+    writer = SummaryWriter("Loss_Function")
+    for p in range(5):
+        for i in range(0, BUFFER_MAX):
+            # ran_idx = torch.randint(low=0, high=4000, size=(1,))
             pl_req = planning_requests[i]
-            SV = pl_req.start
-            SV = torch.tensor(SV)
-            # print("start vektor:", SV)
-            # print("Original shape:", SV.shape)
-            GV = pl_req.goal
-            GV = torch.tensor(GV)
-            # print("goal vektor:", GV)
-            # print("Original shape:", GV.shape)
-            req = dataset[i]
-            OC = pl.get_occupancy_map(req)
-            OC = torch.tensor(OC)
+            OC = pl.get_occupancy_map(pl_req)
             OC = OC.reshape([1, OC.shape[0], -1])
-            # print("occupancy map:", OC)
-            # print("Original shape:", OC.shape)
+            print("oc size:", OC.shape)
+            SV = pl_req.start
+            GV = pl_req.goal
             pl.generate_graph_halton(150)
-            pr = pl.search(req)
+            pr = pl.search(pl_req)
             # VALUE_ESTIMATE = pr.checked_counts
             # VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE)
-            # print("Original shape:", VALUE_ESTIMATE.shape)
-            W = gen_model(OC, SV, GV)
+            OC = torch.tensor(OC).to(device)
+            SV = torch.tensor(SV).to(device)
+            GV = torch.tensor(GV).to(device)
+            diri_dist = gen_model(OC, SV, GV)
+            W = diri_dist.sample()
+
+            # print("W", W, W.shape)
             gmm_dist = gmm_dist_generator(W)
-            W = torch.tensor(W)
-            # print("distribution weight:", W)
-            # print("Original shape:", W.shape)
-
-            MyValidStateSampler = MyValidStateSampler(gmm_dist, pl_req)
-            VALUE_ESTIMATE = MyValidStateSampler.plan()
-            print("VALUE_ESTIMATE", VALUE_ESTIMATE)
+            VALUE_ESTIMATE = plan(pl_req, gmm_dist)[0]
+            VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE).to(device)
+            # print("GMM maxcount", VALUE_ESTIMATE)
             experience = ([OC, SV, GV, W, VALUE_ESTIMATE])
-
             replay_buffer.append(experience)
-            print('Waiting for buffer size ... {}/{}'.format(len(replay_buffer), BATCH_MAX))
+            VALUE_RAN = random_plan(pl_req)
+            # VALUE_RAN_SUM = VALUE_RAN + VALUE_RAN_SUM
+            # VALUE_ESTIMATE_SUM = VALUE_ESTIMATE.cpu() + VALUE_ESTIMATE_SUM
+            # del MVS, RVS
+            print('Waiting for buffer size ... {}/{}'.format(len(replay_buffer), BUFFER_MAX))
+        for i in range(5):
 
-        for i in range(TRAIN_T):
             # labels = sampled_oc, sampled_start_v, sampled_goal_v, sampled_coefficients, sampled_values
             sampled_evaluations = random.sample(replay_buffer, REPLAY_SAMPLE_SIZE)
+            # print("sssssss", sampled_evaluations)
             # data_labels = {sampled_evaluations[i]: labels[i] for i in range(len(sampled_evaluations))}
             sampled_oc = torch.stack([t[0] for t in sampled_evaluations])
             # print("OC:", sampled_oc, sampled_oc.shape)
@@ -110,75 +106,81 @@ if __name__ == '__main__':
             sampled_values = torch.stack([t[4] for t in sampled_evaluations])
             # print("value:", sampled_values, sampled_values.shape)
 
-            # update Cir
             critic_loss = 0
-
             for j in range(REPLAY_SAMPLE_SIZE):
-                (mean, std) = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j],
-                                           sampled_coefficients[j])
+                # cri_idx = j + (i * REPLAY_SAMPLE_SIZE)
+                mean, std = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j],
+                                         sampled_coefficients[j])
                 std = torch.exp(std)
-                # print("mean:", mean, "std:", std)
+                print("mean:", mean, "std:", std)
                 priori_pro = dist.Normal(mean, std)
                 # print("posterior:", priori_pro)
                 posterior_prob = priori_pro.log_prob(sampled_values[j])
                 # print("posterior_prob:", posterior_prob)
                 # print("test", priori_pro.log_prob(a).exp())
-                critic_loss = critic_loss + (-posterior_prob)
-            # print("critic_loss", critic_loss)
+                critic_loss = critic_loss + (-posterior_prob) / REPLAY_SAMPLE_SIZE
+
             critic_model_optimizer.zero_grad()
-            gen_model_optimizer.zero_grad()
-            critic_loss.backward()
+
+            critic_loss.backward(retain_graph=True)
             torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 1.0)
             critic_model_optimizer.step()
 
             # Update generator
             gen_objective = 0
-            for j in range(REPLAY_SAMPLE_SIZE):
-                mean = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j], sampled_coefficients[j])[0]
-                print("mean_gen", mean)
-                # dir_dist = Dirichlet(sampled_coefficients[j])
-                # entropy = dir_dist.entropy()
+            for k in range(REPLAY_SAMPLE_SIZE):
+                sampled_coefficients, entropy = gen_model.rsample(sampled_oc[k], sampled_start_v[k], sampled_goal_v[k])
+                mean, std = critic_model(sampled_oc[k], sampled_start_v[k], sampled_goal_v[k], sampled_coefficients)
+
+                dir_dist = gen_model(sampled_oc[k], sampled_start_v[k], sampled_goal_v[k])
+
                 # print("entropy", entropy)
-                # dual_terms = (log_alpha.exp().detach() * entropy)
+                dual_terms = (log_alpha.exp().detach() * entropy)
                 # print("dual_term", dual_terms)
-                gen_objective = gen_objective + mean
-                # print("gen_objective", gen_objective)
-            # print("gen_objectivesum", gen_objective)
-            critic_model_optimizer.zero_grad()
+                gen_objective = (gen_objective + mean - dual_terms) / REPLAY_SAMPLE_SIZE
+
+                # gen_objective = (gen_objective + mean) / REPLAY_SAMPLE_SIZE
+
             gen_model_optimizer.zero_grad()
             gen_objective.backward()
             torch.nn.utils.clip_grad_norm_(gen_model.parameters(), 1.0)
             gen_model_optimizer.step()
+            print("gen_objective", gen_objective)
 
-            # update alpha
-            """alpha_loss = 0
-            for j in range(REPLAY_SAMPLE_SIZE):
-                dir_dist = Dirichlet(sampled_coefficients[j])
-                entropy = dir_dist.entropy()
+            # update loss
+            alpha_loss = 0
+            for m in range(REPLAY_SAMPLE_SIZE):
+                # alp_idx = m + REPLAY_SAMPLE_SIZE * i
+                # dir_dist = Dirichlet(sampled_coefficients[j])
+                sampled_coefficients, entropy = gen_model.rsample(sampled_oc[m], sampled_start_v[m], sampled_goal_v[m])
 
+                print("entropy", entropy)
                 alpha_loss_single = log_alpha.exp() * ((entropy - torch.tensor(
                     TARGET_ENTROPY, device=device, dtype=torch.float32)).detach())
-                #  print("1", alpha_loss_single)
-                alpha_loss = alpha_loss + alpha_loss_single
-            # print("2", alpha_loss)
-            # print("3", alpha_loss)
+                alpha_loss = (alpha_loss + alpha_loss_single) / REPLAY_SAMPLE_SIZE
+            critic_model_optimizer.zero_grad()
+            gen_model_optimizer.zero_grad()
             alpha_optim.zero_grad()
             alpha_loss.backward()
             with torch.no_grad():
                 log_alpha.grad *= (((-log_alpha.grad >= 0) | (log_alpha >= LOG_ALPHA_MIN)) &
                                    ((-log_alpha.grad < 0) | (log_alpha <= LOG_ALPHA_MAX))).float()  # ppo
-            alpha_optim.step()"""
+            alpha_optim.step()
 
-            # critic_loss = critic_loss.detach().numpy()
-            critic_loss = critic_loss.item()
+            # critic_loss = critic_loss.item()
             gen_objective = gen_objective.item()
-            # alpha_loss = alpha_loss.item()
-            writer.add_scalar("CRITIC LOSS", critic_loss, i)
-            writer.add_scalar("GEN LOSS", gen_objective, i)
-            # writer.add_scalar("ALPHA LOSS", alpha_loss, i)
+            alpha_loss = alpha_loss.item()
+            # loss function visualable
+            # writer.add_scalar("CRITIC LOSS", critic_loss)
+            writer.add_scalar("GEN LOSS", gen_objective)
+            writer.add_scalar("ALPHA LOSS", alpha_loss)
+        writer.close()
 
-    torch.save(critic_model, 'net.critic')
-    torch.save(gen_model, 'net.generator')
 
-    writer.close()
-    # tensorboard --logdir=/home/wangkaige/Project/apes/LOSS_FUNCTION
+
+
+
+
+    
+    
+    # tensorboard --logdir=/home/wangkaige/Project/apes/LOSS_FUNCTION """
--- network_2d.py
+++ network_2d.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch.distributions.dirichlet import Dirichlet
 
 
 # def LeakyReLU(x, x_max=1, hard_slope=1e-2):
@@ -11,28 +12,25 @@ import numpy as np
 class APESCriticNet(nn.Module):
     def __init__(self):
         super(APESCriticNet, self).__init__()
-        self.mp = torch.nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.conv2 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.conv3 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.ReLU = torch.nn.ReLU()
-        self.fc1 = nn.Linear(630, 300)  # 1728改为1728+start+goal
-        self.fc2 = nn.Linear(300, 300)
-        self.fc3 = nn.Linear(300, 300)
-        self.fc4 = nn.Linear(300, 2)
+        self.mp = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=2, padding=0)
+        self.conv2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=0)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0)
+        self.LeakyReLU = torch.nn.LeakyReLU(inplace=False)
+        self.fc1 = nn.Linear(1654, 512)  # 1728改为1728+start+goal
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 512)
+        self.fc4 = nn.Linear(512, 2)
 
     def forward(self, x, s, g, coefficients):
-        # x = x.reshape((x.shape[0], -1, self.particle_size))
         x = self.conv1(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
-
         x = self.conv2(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
-
         x = self.conv3(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
         x = torch.flatten(x)
         s = torch.flatten(s)
@@ -40,15 +38,16 @@ class APESCriticNet(nn.Module):
         coefficients = torch.flatten(coefficients)
         x = torch.cat((x, s, g, coefficients), 0)
         x = self.fc1(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc2(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc3(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc4(x)
         # x = x.cpu()
         # x = x.detach()
-        # x = x.numpy().tolist
+        # x = x.numpy().tolist()
+
         return x
 
 
@@ -56,41 +55,56 @@ class APESCriticNet(nn.Module):
 class APESGeneratorNet(nn.Module):
     def __init__(self):
         super(APESGeneratorNet, self).__init__()
-        self.mp = torch.nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.conv2 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.conv3 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, stride=1, padding=0)
-        self.ReLU = torch.nn.ReLU()
-        self.fc1 = nn.Linear(580, 300)  # 1728改为1728+start+goal
-        self.fc2 = nn.Linear(300, 300)
-        self.fc3 = nn.Linear(300, 300)
-        self.fc4 = nn.Linear(300, 50)
+        self.mp = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=2, padding=0)
+        self.conv2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=0)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0)
+        self.LeakyReLU = torch.nn.LeakyReLU(inplace=False)
+        self.fc1 = nn.Linear(1604, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 512)
+        self.fc4 = nn.Linear(512, 50)
 
     def forward(self, x, s, g):
         x = self.conv1(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
         x = self.conv2(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
         x = self.conv3(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.mp(x)
         x = torch.flatten(x)
         s = torch.flatten(s)
         g = torch.flatten(g)
         x = torch.cat((x, s, g), 0)
         x = self.fc1(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc2(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc3(x)
-        x = self.ReLU(x)
+        x = self.LeakyReLU(x)
         x = self.fc4(x)
         x = torch.exp(x)
-        x = x.clone().detach().cpu().numpy()
-        return np.random.dirichlet(x, size=1)
+        print("x", x)
+        dist = Dirichlet(x)
 
+        return dist
+
+    def rsample(self, x, s, g):
+        dist = self.forward(x, s, g)
+        dirchlet_sample = dist.rsample()
+        dirchlet_entropy = dist.entropy()
+
+        return dirchlet_sample, dirchlet_entropy
+
+    # def sample(self, x, s, g):
+    # coefficients_dist = self.forward(x, s, g)
+    # coefficients_rs = coefficients_dist.rsample()
+    # coefficients_entropy = -coefficients_dist.prob(coefficients_rs) * coefficients_dist.log_prob(coefficients_rs)
+
+    # return coefficients_entropy
     # def sample(self, x, s, g):
     # coefficients_dist = self.forward(x, s, g)
     # coefficients_rs = coefficients_dist.rsample()
